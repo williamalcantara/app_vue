bf6d55f050ea42477b9159f03cc6c75109de01ce
William <williamramom@hotmail.com>
2023-08-22 19:54:03 -0300

    Criei Header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <div class="header">
+            header
+        </div>
+    </header>
+        
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+<style>
+    .header{
+       background: #000;
+       color: #fff; 
+    }
+</style>
